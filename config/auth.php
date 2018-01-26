b797dc8cac85d74abb4138e41d484cee3a8424d4
PCHAYDER\Hayder <hayderespit@gmail.com>
2018-01-25 20:57:26 -0500

    login

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Restaurante\Configuracion\User::class,
         ],
 
         // 'users' => [
